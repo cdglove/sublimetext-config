4ff3c678989ca05053db6e5606747172fb522ba1
Chris Glover <c.d.glover@gmail.com>
2015-10-05 16:31:14 -0400

    Add ability to open in visual studio.

---
+++ openinvs.py
@@ -0,0 +1,16 @@
+import sublime, sublime_plugin, os, subprocess
+
+class OpenInVsCommand(sublime_plugin.TextCommand):
+    def run( self, edit, ):
+
+        if self.view.file_name():
+            command = [ "C:/Program Files (x86)/Microsoft Visual Studio 11.0/Common7/IDE/devenv.exe", "/Edit", self.view.file_name() ]
+            p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+            result, err = p.communicate()
+
+            self.view.set_status('oia',result+err)
+            sublime.set_timeout(self.clear,2000)
+
+    def clear(self):
+        self.view.erase_status('p4')
